3ab4aa21bba11b804326f3252f7295b86434c0df
Kevin <dragn194@gmail.com>
2017-03-31 19:21:45 -0400

    Add a board object to the scene

---
+++ Assets/Board.cs
@@ -0,0 +1,30 @@
+﻿using Assets.defs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerateMap()
+    {
+        GameObject orePrefab = LoadTilePrefab(ResourceTypes.Ore);
+        Instantiate(orePrefab, this.transform);
+    }
+
+    private GameObject LoadTilePrefab(ResourceTypes resource)
+    {
+        string resourceStr = resource.ToString().ToLower();
+        return Resources.Load<GameObject>(string.Format("terrain/{0}/tile_{0}", resourceStr));
+    }
+
+}
