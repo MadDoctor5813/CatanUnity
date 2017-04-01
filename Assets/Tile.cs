dc2542697a2cc4677a8fc8c8c9a46cb71ec1bd31
Kevin <dragn194@gmail.com>
2017-04-01 12:44:47 -0400

    Add a Tile script

---
+++ Assets/Tile.cs
@@ -0,0 +1,21 @@
+﻿using Assets.defs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    public ResourceTypes Type;
+    public Vector2 HexCoords;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
