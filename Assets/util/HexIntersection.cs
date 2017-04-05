77835ef8e6edbcfd5ea0e8f35bca810b99c5ef5a
Kevin <dragn194@gmail.com>
2017-04-04 22:02:59 -0400

    Add a HexIntersection class

---
+++ Assets/util/HexIntersection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.util
+{
+    public class HexIntersection
+    {
+
+        public HexCoords[] Neighbors { get; set; }
+
+        public HexIntersection(HexCoords h1, HexCoords h2, HexCoords h3)
+        {
+            Neighbors = new HexCoords[3];
+            Neighbors[0] = h1;
+            Neighbors[1] = h2;
+            Neighbors[2] = h3;
+        }
+
+    }
+}
