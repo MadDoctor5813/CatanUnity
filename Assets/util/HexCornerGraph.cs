89d356faa3bc5b9d7eae62806ce4d53a8f2711a7
Kevin <dragn194@gmail.com>
2017-04-12 18:09:08 -0400

    Add a method to find the distance between two HexCorners in the graph
    It uses breadth first search, and it worked once, so I'm committing it.

--- Assets/util/HexCornerGraph.cs
+++ Assets/util/HexCornerGraph.cs
@@ -18,6 +18,39 @@ namespace Assets.util
             Build(new HexCorner(new HexCoords(0, 0), new HexCoords(0, 1), new HexCoords(-1, 1)));
         }
 
+        public int CornerDistance(HexCorner start, HexCorner end)
+        {
+            Dictionary<HexCorner, HexCorner> parents = new Dictionary<HexCorner, HexCorner>();
+            Queue<HexCorner> queue = new Queue<HexCorner>();
+            queue.Enqueue(start);
+            while (queue.Count != 0)
+            {
+                HexCorner current = queue.Dequeue();
+                if (current.Equals(end))
+                {
+                    break;
+                }
+                HexCorner[] currNeighbors = Neighbors[current];
+                for (int i = 0; i < currNeighbors.Length; i++)
+                {
+                    if (!parents.ContainsKey(currNeighbors[i]))
+                    {
+                        parents.Add(currNeighbors[i], current);
+                        queue.Enqueue(currNeighbors[i]);
+                    }
+                }
+            }
+            //find distance
+            int distance = 0;
+            HexCorner pathNode = end;
+            while (!pathNode.Equals(start))
+            {
+                pathNode = parents[pathNode];
+                distance++;
+            }
+            return distance;
+        }
+
         private void Build(HexCorner corner)
         {
             //dont add duplicates
