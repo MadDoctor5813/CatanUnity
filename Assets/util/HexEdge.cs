4ecd073864a8fa02d91dccf00972b8d190541790
Kevin <dragn194@gmail.com>
2017-04-19 22:08:08 -0400

    Add HexEdge class

---
+++ Assets/util/HexEdge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.util
+{
+    class HexEdge
+    {
+        HexCorner Start { get; set; }
+        HexCorner End { get; set; }
+    }
+}
