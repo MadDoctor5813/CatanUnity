11e652e7dc4878855817f5f842e60a88d73bd907
Kevin <dragn194@gmail.com>
2017-04-19 22:19:01 -0400

    Add a Road script

---
+++ Assets/units/road/Road.cs
@@ -0,0 +1,21 @@
+﻿using Assets.util;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+
+    public HexEdge Edge;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+}
