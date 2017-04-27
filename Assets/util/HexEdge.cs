f9a33371753dc4e8e5ed1a9ceee03b31a92e3e21
Kevin <dragn194@gmail.com>
2017-04-27 18:45:06 -0400

    Add a constructor and coordinate and rotation methods to HexEdge

--- Assets/util/HexEdge.cs
+++ Assets/util/HexEdge.cs
@@ -11,6 +11,17 @@ namespace Assets.util
         HexCorner Start { get; set; }
         HexCorner End { get; set; }
 
+        public HexEdge(HexCorner start, HexCorner end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public static Vector3 ToLocalCoords(HexEdge edge)
+        {
+            return (HexCorner.ToLocalCoords(edge.Start) + HexCorner.ToLocalCoords(edge.End)) / 2;
+        }
+
         public override bool Equals(object obj)
         {
             if (obj is HexEdge)
@@ -24,10 +35,41 @@ namespace Assets.util
             }
         }
 
+        public static Quaternion GetRotation(HexEdge edge)
+        {
+            Vector3 start = HexCorner.ToLocalCoords(edge.Start);
+            Vector3 end = HexCorner.ToLocalCoords(edge.End);
+            Vector3 diff = start - end;
+            float angle = Mathf.Rad2Deg * Mathf.Atan2(diff.z, diff.x);
+            //move the angle returned from atan2 into a 0-360 range
+            if (angle > 90)
+            {
+                angle = 450 - angle;
+            }
+            else
+            {
+                angle = 90 - angle;
+            }
+            return Quaternion.AngleAxis(angle, Vector3.up);
+        }
+
+        public static HexEdge GetNearestEdge(Vector3 coords)
+        {
+            HexCorner corner = HexCorner.GetNearestCorner(coords);
+            HexCorner[] neighbors = HexCorner.GetNeighbors(corner);
+            float[] distances = new float[neighbors.Length];
+            for (int i = 0; i < neighbors.Length; i++)
+            {
+                distances[i] = Vector3.Distance(coords, HexCorner.ToLocalCoords(neighbors[i]));
+            }
+            return new HexEdge(corner, neighbors[Array.IndexOf(distances, Mathf.Min(distances))]);
+        }
+
         public override int GetHashCode()
         {
             return Start.GetHashCode() ^ End.GetHashCode();
         }
+
     }
 
 }
