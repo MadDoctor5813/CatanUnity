0fd5964828b9f7def6018917b05ed8cd529fed38
Kevin <dragn194@gmail.com>
2017-05-17 23:13:36 -0400

    Add IAction interface
    This interface will be used for every action a player can take in the
    game. The Display method displays the result of the action in the
    BoardView, while the Undo method reverses what the Display method does.
    The Apply method finalizes the Action and puts the changes into the
    Board class.

---
+++ Assets/action/IAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.action
+{
+    public interface IAction
+    {
+
+        void Display(BoardView boardView);
+        void Apply(BoardView boardView);
+        void Undo(BoardView boardView);
+    
+    }
+}
