35ffc3756f1219d56f0142e55c053b144f61b117
Kevin <dragn194@gmail.com>
2017-05-17 23:13:46 -0400

    Add an action for placing settlements

---
+++ Assets/action/PlaceSettlementAction.cs
@@ -0,0 +1,47 @@
+﻿using Assets.defs;
+using Assets.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.action
+{
+    class PlaceSettlementAction : IAction
+    {
+
+        public HexCorner Location { get; set; }
+        public PlayerColor Color { get; set; }
+
+        public Unit Settlement { get; set; }
+
+        public PlaceSettlementAction(HexCorner location, PlayerColor color)
+        {
+            Location = location;
+            Color = color;
+            Settlement = null;
+        }
+
+        public void Display(BoardView boardView)
+        {
+            if (boardView.Board.IsValidSettlement(Location))
+            {
+                Settlement = boardView.InstantiateSettlement(Location, Color);
+            }
+        }
+
+        public void Apply(BoardView boardView)
+        {
+            boardView.Board.AddUnit(Location, Settlement);
+        }
+
+        public void Undo(BoardView boardView)
+        {
+            if (Settlement != null)
+            {
+                GameObject.Destroy(Settlement.gameObject);
+            }
+        }
+    }
+}
